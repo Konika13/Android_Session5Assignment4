93f59b56ec20c2833d8853105834dc6578c21b9e
Konika13 <konika.agarwal1994@gmail.com>
2017-01-24 02:25:19 +0530

    Add files via upload

---
+++ script5.4.js
@@ -0,0 +1,11 @@
+
+var arr=[{'name':'Konika Agarwal','age':22,'salary':'1.5 lac','city':'Rampur','state':'Uttar Pradesh'},{'name':'Shivangi','age':21},{'name':'Urvashi Gwal','age':23},{'name':'Ankita Goswami','age':20}];
+console.log("Details of first employee-")
+console.log(arr[0].name)
+console.log(arr[0].age)
+console.log(arr[0].salary)
+console.log(arr[0].city)
+console.log(arr[0].state)
+console.log("Few details of second employee-")
+console.log(arr[1].name)
+console.log(arr[1].age)
